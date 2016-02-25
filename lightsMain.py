902317c3320b16f67e10437ccf61b9d506ad8047
mitchamtuell <mitchamtuell@gmail.com>
2016-02-25 18:44:39 -0500

    button test

--- lightsMain.py
+++ lightsMain.py
@@ -8,22 +8,38 @@ blue = 3
 black = 14
 gold = 4
 
+down = 17
+up = 27
+onoff = 22
+
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
 GPIO.setup(black, GPIO.OUT)
 GPIO.setup(gold, GPIO.OUT)
 
+GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(onoff, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 while True:
-    GPIO.output(red, False)
-    GPIO.output(blue, False)
-    GPIO.output(black, False)
-    GPIO.output(gold, False)
+    upIn = GPIO.input(up)
+    downIn = GPIO.input(down)
+    onoffIn = GPIO.input(onoff)
 
-    time.sleep(1)
+    if upIn:
+        GPIO.output(red, False)
+    else:
+        GPIO.output(red, True)
 
-    GPIO.output(red, True)
-    GPIO.output(blue, True)
-    GPIO.output(black, True)
-    GPIO.output(gold, True)
+    if downIn:
+        GPIO.output(blue, False)
+    else:
+        GPIO.output(blue, False)
 
-    time.sleep(1)
+    if onoffIn:
+        GPIO.output(black, False)
+    else:
+        GPIO.output(black, False)
+
+    time.sleep(0.1)
+
