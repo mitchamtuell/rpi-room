4eac1f3044535129bff409adbbd30545bf5bda53
mitchamtuell <mitchamtuell@gmail.com>
2016-02-25 19:04:04 -0500

    v1 of real program

--- lightsMain.py
+++ lightsMain.py
@@ -21,25 +21,52 @@ GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(onoff, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
+state = 4
+oldstate = 0
+
+while True:                         # Main loop
     upIn = GPIO.input(up)
     downIn = GPIO.input(down)
     onoffIn = GPIO.input(onoff)
 
-    if upIn:
-        GPIO.output(red, True)
-    else:
+    if state == 0:                  # All lights off
         GPIO.output(red, False)
-
-    if downIn:
-        GPIO.output(blue, True)
-    else:
         GPIO.output(blue, False)
-
-    if onoffIn:
-        GPIO.output(black, True)
-    else:
         GPIO.output(black, False)
+        GPIO.output(gold, False)
+    elif state == 1:                # Red on
+        GPIO.output(red, True)
+        GPIO.output(blue, False)
+        GPIO.output(black, False)
+        GPIO.output(gold, False)
+    elif state == 2:                # Red, Blue on
+        GPIO.output(red, True)
+        GPIO.output(blue, True)
+        GPIO.output(black, False)
+        GPIO.output(gold, False)
+    elif state == 3:                # Red, Blue, Black on
+        GPIO.output(red, True)
+        GPIO.output(blue, True)
+        GPIO.output(black, True)
+        GPIO.output(gold, False)
+    elif state == 4:                # All lights on
+        GPIO.output(red, True)
+        GPIO.output(blue, True)
+        GPIO.output(black, True)
+        GPIO.output(gold, True)
+
+    if upIn:                        # Handle button presses
+        if state < 4:
+            state += 1
+    if downIn:
+        if state > 1:
+            state -= 1
+    if onoffIn:
+        if state > 0:
+            oldstate = state
+            state = 0
+        elif state == 0:
+            state = oldstate
 
     time.sleep(0.1)
 
